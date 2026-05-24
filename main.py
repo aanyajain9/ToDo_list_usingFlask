7d475735556d8ae192884eb6a8ec370ac9026ab5
aanyajain9 <aanyajain661@gmail.com>
2026-05-24 18:57:30 +0530

    implemented form functionality

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template , request
+from flask import Flask, render_template , request , redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 app = Flask(__name__)   
@@ -16,13 +16,13 @@ class Todo(db.Model):
 '''app means they are talking about website'''
 @app.route("/",methods=['get','POST'])
 def helloworld():
-    if request.method=='POST':
-        title=(request.form['title'])
-        desc=(request.form['desc'])
+    if request.method=="POST":
+        title=request.form['title']
+        desc=request.form['desc']
         todo =Todo(title=title,desc=desc)
         db.session.add(todo)
         db.session.commit()
-        allTodo=Todo.query.all()
+    allTodo=Todo.query.all()
     return render_template('index.html',allTodo=allTodo)
 
 @app.route('/show')
@@ -30,6 +30,19 @@ def products():
     allTodo= Todo.query.all()
     print(allTodo)
     return 'this is product page'
+
+@app.route('/update/<int:sno>')
+def update(sno):
+    allTodo= Todo.query.all()
+    print(allTodo)
+    return 'this is product page'
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo= Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
     
 if __name__=="__main__":
     app.run(debug=True,port =8000)
